93350572d0a54cb59ccbf0783fe3f57ef0d5a932
mcrymbo <croco2023@gmail.com>
2024-03-12 11:46:18 +0300

    eslint fix

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,10 @@
 export default function updateUniqueItems(groceries) {
-  if (!(groceries instanceof Map)) return 'Cannot process';
+  if (!(groceries instanceof Map)) {
+    throw Error('Cannot process');
+  }
 
-  groceries.forEach((value, key) => {
-    if (value === 1) {
+  groceries.forEach((val, key) => {
+    if (val === 1) {
       groceries.set(key, 100);
     }
   });
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set].filter((item) => item.startsWith(startString))
+  return [...set]
+    .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length))
     .join('-');
 }
